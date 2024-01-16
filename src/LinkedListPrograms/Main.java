b5f7e899a54e0013c6b669dfc7d3c038af244ea1
Sion <schow34@safeway.com>
2024-01-16 21:35:22 +0530

    Consists of prepend, remove first and getIndex methods in LL

--- src/LinkedListPrograms/Main.java
+++ src/LinkedListPrograms/Main.java
@@ -2,16 +2,25 @@ package LinkedListPrograms;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList1 linkedList1 = new LinkedList1(4);
-        linkedList1.append(3);
+        LinkedList1 linkedList1 = new LinkedList1(20);
+        linkedList1.append(19);
 
         System.out.println("\nLinked List: before removing the last");
         linkedList1.printList();
         System.out.println("\nLinked List: after removing the last:");
-//        removed 3
+//        removed 19
         linkedList1.removeLast();
-//        removed 4
+//        removed 20
         linkedList1.removeLast();
         linkedList1.printList();
+
+        System.out.println("\nLinked List: after prepending:");
+        linkedList1.append(19);
+        linkedList1.prepend(18);
+        linkedList1.printList();
+
+        int index = 0;
+        System.out.println("\nLinked List: getting node at index "+index+" is ");
+        System.out.println(linkedList1.getIndex(index).value);
     }
 }
