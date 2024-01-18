b50aec17f5cb152fc48736052cacb2c62f12a557
Sion <sionc140720@gmail.com>
2024-01-18 21:13:00 +0530

    added remove,reverse sibgle LL, set and insert

--- src/LinkedListPrograms/Main.java
+++ src/LinkedListPrograms/Main.java
@@ -22,5 +22,27 @@ public class Main {
         int index = 0;
         System.out.println("\nLinked List: getting node at index "+index+" is ");
         System.out.println(linkedList1.getIndex(index).value);
+
+        int value = 20;
+        int index2 = 1;
+        System.out.println("\nLinked List: setting node at index "+index2+" of value "+value+" is ");
+        linkedList1.set(index2,value);
+        linkedList1.printList();
+
+
+        int index3 = 2;
+        int value2 = 21;
+        System.out.println("\nLinked List: inserting node at index "+index3+" of value "+value2+" is ");
+        linkedList1.insert(index3,value2);
+        linkedList1.printList();
+
+
+        System.out.println("\nLinked List: removing node at index "+ 1);
+        linkedList1.remove(1);
+        linkedList1.printList();
+
+        System.out.println("\nLinked List: reversing a linked list");
+        linkedList1.reverse();
+        linkedList1.printList();
     }
 }
