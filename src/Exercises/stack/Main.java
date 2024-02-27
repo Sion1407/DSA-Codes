b82c8d324c6c84288d063d830fbec5552d0bfa65
Sion <schow34@safeway.com>
2024-02-27 22:24:16 +0530

    Added stack and queue operations

--- src/Exercises/stack/Main.java
+++ src/Exercises/stack/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Stack stack1 = new Stack(1);
         stack1.printStack();
+
+        System.out.println("Pusing an element at top in stack");
+        stack1.push(2);
+        stack1.printStack();
+
+        System.out.println("popping an element from stack");
+        stack1.pop();
+        stack1.printStack();
     }
 }
