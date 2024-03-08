dea9665bdfb122b883afa73268df7aa48011e307
Sion <schow34@safeway.com>
2024-03-08 12:24:26 +0530

    Added get and set methods in hashtable

--- src/Exercises/hashTable/Main.java
+++ src/Exercises/hashTable/Main.java
@@ -3,6 +3,12 @@ package Exercises.hashTable;
 public class Main {
     public static void main(String[] args) {
         HashTable hashTable = new HashTable();
+        hashTable.set("nails",100);
+        hashTable.set("screws",200);
+        hashTable.set("adsra",300);
         hashTable.printTable();
+
+        System.out.println("searching in hashtable for value of nails: "+ hashTable.get("adsra"));
+
     }
 }
